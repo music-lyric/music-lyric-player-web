22607f2b6fd414640ed0da5afb484d80b57649ca
folltoshe <folltoshe@foxmail.com>
2026-04-19 17:21:02 +0800

    refactor(utils): deep type tools

--- packages/utils/src/types/deep.ts
+++ packages/utils/src/types/deep.ts
@@ -3,39 +3,43 @@ type SkipDeepBuiltins = RegExp | Date | Error | Blob | File | URL | ArrayBuffer 
 export type DeepRequired<T> = T extends (...args: any[]) => any
   ? T
   : T extends SkipDeepBuiltins
-  ? NonNullable<T>
-  : T extends Promise<infer U>
-  ? Promise<DeepRequired<NonNullable<U>>>
-  : T extends readonly any[]
-  ? { [K in keyof T]-?: DeepRequired<NonNullable<T[K]>> }
-  : T extends Array<infer U>
-  ? Array<DeepRequired<NonNullable<U>>>
-  : T extends Map<infer K, infer V>
-  ? Map<DeepRequired<NonNullable<K>>, DeepRequired<NonNullable<V>>>
-  : T extends Set<infer U>
-  ? Set<DeepRequired<NonNullable<U>>>
-  : T extends object
-  ? { [K in keyof T]-?: DeepRequired<NonNullable<T[K]>> }
-  : NonNullable<T>
+    ? T
+    : T extends Promise<infer U>
+      ? Promise<DeepRequired<U & {}>>
+      : T extends Map<infer K, infer V>
+        ? Map<DeepRequired<K & {}>, DeepRequired<V & {}>>
+        : T extends Set<infer U>
+          ? Set<DeepRequired<U & {}>>
+          : T extends readonly any[]
+            ? { [K in keyof T]-?: DeepRequired<T[K] & {}> }
+            : T extends object
+              ? { [K in keyof T]-?: DeepRequired<T[K] & {}> }
+              : T & {}
 
 export type DeepPartial<T> = T extends (...args: any[]) => any
   ? T
-  : T extends Map<infer K, infer V>
-  ? Map<DeepPartial<K>, DeepPartial<V>>
-  : T extends Set<infer U>
-  ? Set<DeepPartial<U>>
-  : T extends object
-  ? { [K in keyof T]?: DeepPartial<T[K]> }
-  : T
+  : T extends SkipDeepBuiltins
+    ? T
+    : T extends Map<infer K, infer V>
+      ? Map<DeepPartial<K>, DeepPartial<V>>
+      : T extends Set<infer U>
+        ? Set<DeepPartial<U>>
+        : T extends readonly any[]
+          ? { [K in keyof T]?: DeepPartial<T[K]> }
+          : T extends object
+            ? { [K in keyof T]?: DeepPartial<T[K]> }
+            : T
 
 export type DeepReadonly<T> = T extends (...args: any[]) => any
   ? T
-  : T extends Array<infer U>
-  ? ReadonlyArray<DeepReadonly<U>>
-  : T extends Map<infer K, infer V>
-  ? ReadonlyMap<DeepReadonly<K>, DeepReadonly<V>>
-  : T extends Set<infer U>
-  ? ReadonlySet<DeepReadonly<U>>
-  : T extends object
-  ? { readonly [P in keyof T]: DeepReadonly<T[P]> }
-  : T
+  : T extends SkipDeepBuiltins
+    ? Readonly<T>
+    : T extends Map<infer K, infer V>
+      ? ReadonlyMap<DeepReadonly<K>, DeepReadonly<V>>
+      : T extends Set<infer U>
+        ? ReadonlySet<DeepReadonly<U>>
+        : T extends readonly any[]
+          ? { readonly [K in keyof T]: DeepReadonly<T[K]> }
+          : T extends object
+            ? { readonly [K in keyof T]: DeepReadonly<T[K]> }
+            : T
