255f925e4249c3d04e75c01eb8cfb0bcf5f01659
folltoshe <folltoshe@foxmail.com>
2026-05-05 18:24:55 +0800

    refactor(dom): word space in syllable line

--- packages/dom/src/components/line/normal/main/index.ts
+++ packages/dom/src/components/line/normal/main/index.ts
@@ -174,21 +174,33 @@ export class MainElement {
   private init() {
     this.clear()
 
+    let pending = false
     for (const item of this.info.content.words) {
       switch (item.type) {
         case WordType.Normal: {
           const node = new WordElement(this.context, item, this.info)
+
+          if (pending) {
+            node.element.classList.add(styles.spaceStart)
+            pending = false
+          }
+
           this.words.push(node)
-          this.dom.appendChild(node.element)
           break
         }
         case WordType.Space: {
-          const node = document.createElement('div')
-          applyClassName(node, [styles.space])
-          this.dom.appendChild(node)
+          const prev = this.words[this.words.length - 1]
+          if (prev) {
+            prev.element.classList.add(styles.spaceEnd)
+          }
+          pending = true
           break
         }
       }
+    }
+
+    for (const word of this.words) {
+      this.dom.appendChild(word.element)
     }
 
     this.updateSize()
